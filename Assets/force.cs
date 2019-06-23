8123ced827206721e5f8a1d69751cccd3a16d064
V-coder036 <vikrantshettty036@gmail.com>
2019-06-23 13:49:15 +0530

    Yeah Done

---
+++ Assets/force.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour {
+	public float speed;
+	Vector3 pos;
+	public static int x;
+	// Use this for initialization
+	void Start () {
+		speed = 3f;
+		x = 0;
+		pos = transform.position;
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (x % 2 == 0) {
+			transform.Translate (speed* Time.deltaTime, 0f, 0f);
+			//transform.position = pos;
+		}
+
+		else{
+			transform.Translate (-(speed * 2* Time.deltaTime), 0f, 0f);
+			transform.position = pos;
+		}
+		x++;
+	}
+}
---
+++ Assets/forceBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forceBox : MonoBehaviour {
+	public float speed;
+	public static int x;
+	// Use this for initialization
+	void Start () {
+		speed = 5f;
+		x = 0;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (x % 2 == 0)
+			transform.Translate (speed * Time.deltaTime, 0f, 0f);
+		else
+			transform.Translate (-(speed * Time.deltaTime), 0f, 0f);
+	}
+}
